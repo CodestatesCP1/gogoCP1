52c7dfd9d0211e02344b2950811f604507cfd2be
neemomo <91307272+neemomo@users.noreply.github.com>
2022-09-08 17:48:47 +0900

    snake-game ver.0.0.0

---
+++ snake-game/game.py
@@ -0,0 +1,174 @@
+import os
+import numpy as np
+import time
+import pygame
+
+from gameclasses import Snake, Button
+
+
+def make_feed(snake_body, board_size):
+    x = -1
+    y = -1
+    while not (0 <= x < board_size and 0 <= y < board_size) or (x, y) in snake_body:
+        x = np.random.randint(0, board_size)
+        y = np.random.randint(0, board_size)
+    return x, y
+
+
+def render_score(score, font):
+    txt_score = font.render(score, True, (0, 0, 0))
+    rect_score = txt_score.get_rect(center=(screen_width // 2, (screen_height - screen_width) // 2))
+    screen.blit(txt_score, rect_score)
+
+
+def mode_select():
+    global PLAYER
+
+    btn_width = 300
+    btn_height = 100
+    player_button = Button(screen_width // 2, 2 * screen_height // 5, btn_width, btn_height, '직접 플레이(enter)', btn_font)
+    ai_button = Button(screen_width // 2, 3 * screen_height // 5, btn_width, btn_height, 'AI(미구현)', btn_font)
+
+    running = True
+    while running:
+        if player_button.draw(screen):
+            PLAYER = 'human'
+            running = False
+        if ai_button.draw(screen):
+            print('미구현')
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    PLAYER = 'human'
+                    running = False
+
+        pygame.display.update()
+
+
+def play():
+    # 뱀 최초 위치 랜덤 생성(벽에 너무 가까이 있으면 시작하자마자 죽을 수 있으므로 벽과 적당히 떨어져있도록 설정)
+    snake_init_pos = (
+        np.random.randint(board_size // 4, 3 * board_size // 4),
+        np.random.randint(board_size // 4, 3 * board_size // 4))
+    snake = Snake(position=snake_init_pos, board_size=board_size,
+                  head_img=snake_head, body_img=snake_body, tail_img=snake_tail, img_unit=board_unit_size)
+
+    # 먹이 위치
+    feed_pos = make_feed(snake.get_body(), board_size)
+
+    running = True
+    LU = (0, screen_height - screen_width)
+    score = 0
+    pygame.mixer.music.play(-1)
+    while running:
+        clock.tick(fps)
+
+        next_action = snake.get_direction()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    next_action = 'U'
+                elif event.key == pygame.K_LEFT:
+                    next_action = 'L'
+                elif event.key == pygame.K_DOWN:
+                    next_action = 'D'
+                elif event.key == pygame.K_RIGHT:
+                    next_action = 'R'
+
+        reward = snake.move(next_action, feed_pos)
+        # 먹이 먹음
+        if reward == 1:
+            score += 1
+            feed_pos = make_feed(snake.get_body(), board_size)
+        # 죽음
+        if reward == -1:
+            pygame.mixer.music.stop()
+            break
+
+        screen.blit(background, (0, 0))
+        snake.render(screen, LU)
+        screen.blit(feed, (LU[0] + feed_pos[0] * board_unit_size, LU[1] + feed_pos[1] * board_unit_size))
+        render_score(str(score), game_font)
+        pygame.display.update()
+
+
+def replay():
+    global end
+    pygame.display.update()
+
+    btn_width = 300
+    btn_height = 100
+    replay_button = Button(screen_width // 2, 2 * screen_height // 5, btn_width, btn_height, '다시하기(enter)', btn_font)
+    end_button = Button(screen_width // 2, 3 * screen_height // 5, btn_width, btn_height, '끝내기(esc)', btn_font)
+
+    running = True
+    while running:
+        if replay_button.draw(screen):
+            running = False
+        if end_button.draw(screen):
+            end = True
+            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    running = False
+                if event.key == pygame.K_ESCAPE:
+                    end = True
+                    running = False
+
+        pygame.display.update()
+
+
+# player or ai
+PLAYER = None
+
+pygame.init()
+
+# 보드(정사각형) 한 변을 이루는 칸의 개수
+board_size = 20
+# 보드 한 칸의 한 변 길이
+board_unit_size = 20
+
+screen_width = board_size * board_unit_size
+# 점수 등을 표시할 위쪽 여백 100 확보
+screen_height = screen_width + 100
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('RL Snake')
+clock = pygame.time.Clock()
+# 초당 움직이는 칸 개수
+fps = 7
+
+# 이미지 load
+current_path = os.path.dirname(os.path.realpath(__file__))
+essets_path = f'{current_path}/essets/'
+background = pygame.image.load(os.path.join(essets_path, 'background.jpg'))
+snake_head = pygame.image.load(os.path.join(essets_path, 'snake_head.png')).convert_alpha()
+snake_body = pygame.image.load(os.path.join(essets_path, 'snake_body.png'))
+snake_tail = pygame.image.load(os.path.join(essets_path, 'snake_tail.png')).convert_alpha()
+feed = pygame.image.load(os.path.join(essets_path, 'feed.png'))
+
+background = pygame.transform.scale(background, (screen_width, screen_height))
+snake_head = pygame.transform.scale(snake_head, (board_unit_size, board_unit_size))
+snake_body = pygame.transform.scale(snake_body, (board_unit_size, board_unit_size))
+snake_tail = pygame.transform.scale(snake_tail, (board_unit_size, board_unit_size))
+feed = pygame.transform.scale(feed, (board_unit_size, board_unit_size))
+
+# bgm load
+pygame.mixer.music.load(os.path.join(essets_path + 'bgm.wav'))
+
+btn_font = pygame.font.SysFont('applegothic', 30)
+game_font = pygame.font.SysFont('arialrounded', 40)
+
+mode_select()
+end = False
+while not end:
+    play()
+    replay()
---
+++ snake-game/gameclasses.py
@@ -0,0 +1,153 @@
+import pygame
+import numpy as np
+from collections import deque
+
+
+def to_code(direction):
+    code = None
+    if direction == 'U':
+        code = 0
+    elif direction == 'L':
+        code = 1
+    elif direction == 'D':
+        code = 2
+    elif direction == 'R':
+        code = 3
+    return code
+
+
+class Snake:
+    def __init__(self, position, board_size, head_img, body_img, tail_img, img_unit, init_length=3):
+        self.direction = np.random.choice(['U', 'L', 'D', 'R'])
+
+        dx = -1 if self.direction == 'R' else 1 if self.direction == 'L' else 0
+        dy = -1 if self.direction == 'D' else 1 if self.direction == 'U' else 0
+        self.body = deque()
+        # head
+        self.body.append(position)
+        # body, tail
+        for i in range(1, init_length):
+            self.body.append((position[0] + i * dx, position[1] + i * dy))
+
+        self.board_size = board_size
+        self.head_img = head_img
+        self.body_img = body_img
+        self.tail_img = tail_img
+        self.img_unit = img_unit
+
+    def get_body(self):
+        return self.body
+
+    def get_direction(self):
+        return self.direction
+
+    def move(self, action, feed_position):
+        dir_code = to_code(self.direction)
+        if dir_code is None:
+            return
+        act_code = to_code(action)
+        if act_code is None:
+            return
+
+        # 가던 방향 그대로
+        if dir_code == act_code or (dir_code + 2) % 4 == act_code:
+            act_code = dir_code
+
+        old_head = self.body[0]
+        if act_code == 0:  # Up
+            new_head = (old_head[0], old_head[1] - 1)
+            self.direction = 'U'
+        elif act_code == 1:  # Left
+            new_head = (old_head[0] - 1, old_head[1])
+            self.direction = 'L'
+        elif act_code == 2:  # Down
+            new_head = (old_head[0], old_head[1] + 1)
+            self.direction = 'D'
+        else:  # act_code == 3 -> Right
+            new_head = (old_head[0] + 1, old_head[1])
+            self.direction = 'R'
+
+        # when new_head collides with body or wall
+        if new_head in self.body or not self.validate(self.body[0]):
+            return -1
+
+        self.body.appendleft(new_head)
+
+        # when snake takes feed
+        if new_head == feed_position:
+            return 1
+        else:
+            self.body.pop()
+
+        return 0
+
+    def validate(self, pos):
+        return 0 <= pos[0] < self.board_size and 0 <= pos[1] < self.board_size
+
+    # LU = Left and Up (보드판의 맨 왼쪽 위 좌표)
+    def render(self, screen, LU):
+        L = LU[0]
+        U = LU[1]
+        # head
+        screen.blit(pygame.transform.rotate(self.head_img, to_code(self.direction) * 90),
+                    (L + self.body[0][0] * self.img_unit, U + self.body[0][1] * self.img_unit))
+        # body
+        for i in range(1, len(self.body) - 1):
+            screen.blit(self.body_img, (L + self.body[i][0] * self.img_unit, U + self.body[i][1] * self.img_unit))
+        # tail
+        screen.blit(pygame.transform.rotate(self.tail_img, to_code(self.calc_tail_dir()) * 90),
+                    (L + self.body[-1][0] * self.img_unit, U + self.body[-1][1] * self.img_unit))
+
+    def calc_tail_dir(self):
+        dx = self.body[-1][0] - self.body[-2][0]
+        dy = self.body[-1][1] - self.body[-2][1]
+
+        if dx == 1:
+            return 'R'
+        elif dx == -1:
+            return 'L'
+        elif dy == 1:
+            return 'D'
+        elif dy == -1:
+            return 'U'
+
+
+class Button:
+    def __init__(self, x, y, width, height, button_text, font):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+        self.fill_colors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+        self.txt = font.render(button_text, True, (20, 20, 20))
+        self.rect = self.txt.get_rect(center=(x, y))
+        self.clicked = False
+
+    def draw(self, screen):
+        pos = pygame.mouse.get_pos()
+
+        state = 'normal'
+
+        action = False
+        if self.x - self.width // 2 <= pos[0] < self.x + self.width // 2\
+                and self.y - self.height // 2 <= pos[1] < self.y + self.height // 2:
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                state = 'pressed'
+                action = True
+            else:
+                state = 'hover'
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        pygame.draw.rect(screen, self.fill_colors[state],
+                         [self.x - self.width // 2, self.y - self.height // 2, self.width, self.height])
+        screen.blit(self.txt, self.rect)
+
+        return action
