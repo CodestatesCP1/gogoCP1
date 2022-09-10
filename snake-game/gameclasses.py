489cea4953a191271317b0a91318d549cbcd9fb1
neemomo <91307272+neemomo@users.noreply.github.com>
2022-09-10 19:25:13 +0900

    add volume-control-bar

--- snake-game/gameclasses.py
+++ snake-game/gameclasses.py
@@ -144,14 +144,20 @@ class Button:
         self.clicked = False
         self.rounded_edge = rounded_edge
 
-    def draw(self, screen):
-        pos = pygame.mouse.get_pos()
+    def draw(self, screen, state):
+        border_radius = self.height // 3 if self.rounded_edge else 0
+        pygame.draw.rect(screen, self.fill_colors[state],
+                         [self.x - self.width // 2, self.y - self.height // 2, self.width, self.height],
+                         border_radius=border_radius)
+        screen.blit(self.txt, self.rect)
+
+    def catch_mouse_action(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
         state = 'normal'
-
         action = False
-        if self.x - self.width // 2 <= pos[0] < self.x + self.width // 2\
-                and self.y - self.height // 2 <= pos[1] < self.y + self.height // 2:
+        if self.x - self.width // 2 <= mouse_x < self.x + self.width // 2 \
+                and self.y - self.height // 2 <= mouse_y < self.y + self.height // 2:
             if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 self.clicked = True
                 state = 'pressed'
@@ -162,10 +168,69 @@ class Button:
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
-        border_radius = self.height // 3 if self.rounded_edge else 0
-        pygame.draw.rect(screen, self.fill_colors[state],
-                         [self.x - self.width // 2, self.y - self.height // 2, self.width, self.height],
-                         border_radius=border_radius)
-        screen.blit(self.txt, self.rect)
+        return action, state
 
-        return action
+
+class VolumeControlBar:
+    def __init__(self, x, y, volume_on_img, volume_off_img, bar_width, bar_height, volume=1):
+        self.x = x
+        self.y = y
+        self.volume_on_img = volume_on_img
+        self.volume_off_img = volume_off_img
+        self.img_width = volume_on_img.get_width()
+        self.img_height = volume_on_img.get_height()
+        self.bar_width = bar_width
+        self.bar_height = bar_height
+        self.radius = 1.6 * self.bar_height
+        self.bar_left = self.x + self.img_width + self.radius
+        self.bar_up = self.y + (self.img_height - self.bar_height) // 2
+        self.volume = volume
+        self.mute = False
+
+        self.img_clicked = False
+        self.bar_clicked = False
+
+    def render(self, screen):
+        img = self.volume_on_img if not self.mute else self.volume_off_img
+        screen.blit(img, (self.x, self.y))
+
+        pygame.draw.rect(screen, '#bbbbbb',
+                         (self.bar_left, self.bar_up, self.bar_width, self.bar_height),
+                         border_radius=self.bar_height // 2)
+        if not self.mute:
+            pygame.draw.circle(screen, '#404040',
+                               (self.bar_left + self.volume * self.bar_width, self.bar_up + self.bar_height // 2),
+                               self.radius)
+
+    def catch_mouse_action(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        volume_changed = False
+        if 0 <= mouse_x - self.x < self.img_width and 0 <= mouse_y - self.y < self.img_height \
+                and pygame.mouse.get_pressed()[0] == 1 and not self.img_clicked and not self.bar_clicked:
+            self.img_clicked = True
+            self.mute = not self.mute
+            volume_changed = True
+
+        if not self.bar_clicked and 0 <= mouse_x - self.bar_left < self.bar_width \
+                and 0 <= mouse_y - (self.bar_up + self.bar_height // 2) < self.radius \
+                and pygame.mouse.get_pressed()[0] == 1 and not self.img_clicked:
+            self.bar_clicked = True
+
+        if self.bar_clicked:
+            self.volume = (min(max(mouse_x, self.bar_left), self.bar_left + self.bar_width) - self.bar_left) / self.bar_width
+            if self.volume == 0:
+                self.mute = True
+            elif self.mute:
+                self.mute = False
+            volume_changed = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.img_clicked = False
+            self.bar_clicked = False
+
+        if volume_changed:
+            if self.mute:
+                pygame.mixer.music.set_volume(0)
+            else:
+                pygame.mixer.music.set_volume(self.volume)
